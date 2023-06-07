dc1daba31208815be3c2bca0bc28c5de6d964b96
RodyaPik <116595792+RodyaPik@users.noreply.github.com>
2023-06-07 12:20:12 +0400

    02. Экапсуляция

--- Sem 1/src/App.java
+++ Sem 1/src/App.java
@@ -1,5 +1,11 @@
+import Domen.Product;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Product item1 = new Product(1, "Lays", "Чипсы", 59);
+        item1.setPrice(98);
+
+
+        System.out.println(item1.toString());
     }
 }
