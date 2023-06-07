a7da2b41b77ae56e8079df4b7067d15e2efef838
RodyaPik <116595792+RodyaPik@users.noreply.github.com>
2023-06-07 20:42:51 +0400

    04

--- Sem 1/src/App.java
+++ Sem 1/src/App.java
@@ -1,12 +1,25 @@
 import Domen.Bottle;
 import Domen.Product;
+import VendingMachines.VendingMachine;
 
 public class App {
     public static void main(String[] args) throws Exception { //подключение к продукту
         Product item1 = new Product(1, "Lays", "Чипсы", 59); //заполнения продука
         item1.setPrice(98); //меняем цену
-        Product item2 = new Bottle(2, "Cola", "Water", 70, 50); //второй продукт
+        Product item2 = new Bottle(2, "Cola", "Water", 70, 500); //второй продукт
 
-        System.out.println(item2.toString());
+        VendingMachine iMachine = new VendingMachine(300); //добовление из разных продуктов
+        iMachine.addProduct(item1);
+        iMachine.addProduct(item2);
+        iMachine.addProduct(new Product(3, "Масло", "Масло", 59));// добавили принудительно
+        iMachine.addProduct(new Bottle(4, "Water", "Water", 170, 1500));
+        
+        for(Product prod: iMachine.getProducts())
+        {
+            System.out.println(prod.toString());
+        }
+        
+        
+        
     }
 }
